71b14edb4a41449789c6d22b82fb8c900dea51b7
Victor D <victord@example.com>
2017-03-25 11:05:18 +0200

    little refactoring

--- tasks/victord/task1/main.js
+++ tasks/victord/task1/main.js
@@ -5,7 +5,7 @@ const fs = require('fs');
 const tree = new Map();
 let maxWordLen = 0;
 
-fs.readFileSync(`${__dirname}/../../dict_en.txt`).toString().split(/\r?\n/).forEach(word => {
+for (const word of fs.readFileSync(`${__dirname}/../../dict_en.txt`).toString().split(/\r?\n/)) {
   maxWordLen = Math.max(word.length, maxWordLen);
 
   let subtree = tree;
@@ -16,7 +16,7 @@ fs.readFileSync(`${__dirname}/../../dict_en.txt`).toString().split(/\r?\n/).forE
     subtree = subtree.get(char);
   }
   subtree.set(true, true); // indicate and of a word
-});
+}
 console.log(maxWordLen);
 console.log(tree.get('w').get('o').get('o').get('d').get(true));
 
@@ -34,11 +34,10 @@ function getMatches(text, startIndex) {
     }
   }
 
-  return matches.length > 0 ? matches.reverse() : null;
+  return matches.length > 0 ? matches : null;
 }
 console.log(getMatches('thousandfoldlyabc', 0));
 console.log(getMatches('thousandfoldlyabc', 8));
-console.log(getMatches('tthousandfoldlyabc', 1));
 console.log(getMatches(':-)', 0));
 
 function getGreedySolution(src) {
@@ -47,8 +46,9 @@ function getGreedySolution(src) {
   for (let i = 0; i < src.length; i++) {
     const matches = getMatches(src, i);
     if (matches) {
-      result.push(matches[0]);
-      i += matches[0].length;
+      const bestMatch = matches[matches.length - 1];
+      result.push(bestMatch);
+      i += bestMatch.length;
       i--;
     } else {
       result.push(src[i]);
@@ -60,27 +60,47 @@ function getGreedySolution(src) {
 console.log(getGreedySolution('thisisatest'));
 console.log(getGreedySolution('thousandfoldlyisastrangeword'));
 
-function getAllSolution(src) {
+function getSolutionsTree(src) {
   if (src.length < 2) {
-    return [src];
+    return new Map([[src, new Map()]]);
   }
 
-  const solutionsBySrcLen = new Array(src.length + 1);
-  solutionsBySrcLen[0] = [''];
-  for (let i = 1; i < solutionsBySrcLen.length; i++) {
-    solutionsBySrcLen[i] = [];
+  const solutionsBySrcLen = new Map();
+  for (let i = 0; i <= src.length; i++) {
+    solutionsBySrcLen.set(i, new Map());
   }
 
   for (let i = 0; i < src.length; i++) {
     for (const match of (getMatches(src, i) || [src[i]])) {
-      for (const prevSolution of solutionsBySrcLen[i]) {
-        solutionsBySrcLen[i + match.length].push(`${prevSolution} ${match}`);
+      solutionsBySrcLen.get(i + match.length).set(match, solutionsBySrcLen.get(i));
+    }
+  }
+
+  return solutionsBySrcLen.get(src.length);
+}
+
+function unwindSolutionsTree(tree) {
+  const unwinded = [];
+
+  while (tree.size > 0) {
+    for (let [text, subtree] of tree) {
+      tree.delete(text);
+
+      if (subtree.size === 0) {
+        unwinded.push(text)
+      } else {
+        for (let [subtext, subsubtree] of subtree) {
+          tree.set(`${subtext} ${text}`, subsubtree);
+        }
       }
     }
   }
 
-  return solutionsBySrcLen.pop().sort((s1, s2) => s1.length - s2.length).map(s => s.slice(1));
+  return unwinded;
 }
+console.log(unwindSolutionsTree(getSolutionsTree('t')));
+console.log(unwindSolutionsTree(getSolutionsTree('thisisatest')));
 
-console.log(getAllSolution('thisisatest'));
-console.log(getAllSolution('thousandfoldlyisastrangeword'));
+function getBestSolutionFromTree(tree) {
+  // TODO
+}
