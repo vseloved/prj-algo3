6480aa0d79b4736be075f5f5d241ec07d039579a
Denis Vlasov <denis90@gmail.com>
2017-03-27 14:13:56 +0300

    task 01 using dp

--- tasks/denisvl/01/str-segment.lisp
+++ tasks/denisvl/01/str-segment.lisp
@@ -33,6 +33,51 @@
                    (return-from segment-str (gethash s mem))))))))
 )
 
+
 (print (segment-str "aaaab" (get-test-dict)))
 
 (print (segment-str "helloworld" (load-dict-from-file "../../dict_en.txt")))
+
+
+(defun words-for-position(s pos dict)
+  (loop
+    for len from 1 to (min 10 (- (length s) pos))
+    for prefix = (subseq s pos (+ pos len))
+    when (gethash prefix dict)
+    collect len)
+)
+
+(defun get-dp(s dict)
+  (let ((dp (make-array (+ 1 (length s)) :initial-element nil)))
+    (loop
+      for pos from 0 to (- (length s) 1)
+      for words = (words-for-position s pos dict)
+      when (or (= pos 0) (aref dp pos))
+      do (loop
+           for p in words
+           when (not (aref dp (+ pos p)))
+           do (setf (aref dp (+ pos p)) pos)))
+    (return-from get-dp dp))
+)
+
+(defun get-path(dp)
+  (reverse (loop
+    for p = (- (length dp) 1) then (aref dp p)
+    until (or (= p 0))
+    collect p))
+)
+
+(defun segment-dp(s dict)
+  (let ((dp (get-dp s dict))
+        (pos 0))
+    (if (not (aref dp (length s)))
+      (return-from segment-dp s))
+    (return-from segment-dp (apply #'concatenate 'string (loop
+      for w in (get-path dp)
+      collect (subseq s pos w)
+      when (< w (length s))
+      collect " "
+      do (setf pos w)))))
+)
+
+(print (segment-dp "abaaaabababab" (get-test-dict)))
