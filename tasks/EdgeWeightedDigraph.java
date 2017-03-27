a1722247db8af8fb5b0d43b1513926c17df6edde
dmitriy-panasiuk <dmitriy.panasyuk@gmail.com>
2017-03-27 18:19:28 +0300

    Implemented topological ordering based on DFS

--- tasks/EdgeWeightedDigraph.java
+++ tasks/EdgeWeightedDigraph.java
@@ -1,8 +1,11 @@
+import java.util.ArrayDeque;
 import java.util.ArrayList;
+import java.util.Deque;
 import java.util.HashMap;
 import java.util.HashSet;
 import java.util.List;
 import java.util.Map;
+import java.util.Queue;
 import java.util.Set;
 
 public class EdgeWeightedDigraph {
@@ -13,6 +16,8 @@ public class EdgeWeightedDigraph {
     private List<Set<DirectedEdge>> adj = new ArrayList<>();
     private List<String> vertexValues = new ArrayList<>();
     private Map<String, Integer> valueMappings = new HashMap<>();
+    private Queue<Integer> postorder = new ArrayDeque<>();
+    private boolean[] marked;
 
     public EdgeWeightedDigraph() {
         this.V = 0;
@@ -43,10 +48,44 @@ public class EdgeWeightedDigraph {
         E++;
     }
 
+    private Iterable<DirectedEdge> adj(int v) {
+        return adj.get(v);
+    }
+
     private void validateVertex(String v) {
         if (valueMappings.get(v) == null) {
             throw new IllegalArgumentException("vertex " + v + " does not exist");
         }
+    }
+
+    private void dfs() {
+        marked = new boolean[this.V];
+        for(int v = 0; v < this.V; v++) {
+            if (!marked[v]) {
+                dfs(v);
+            }
+        }
+    }
+
+    private void dfs(int v) {
+        marked[v] = true;
+        for (DirectedEdge e : adj(v)) {
+            int to = e.To();
+            if (!marked[to]) {
+                dfs(to);
+            }
+        }
+        postorder.add(v);
+    }
+
+    public Iterable<Integer> topologicalOrder() {
+        dfs();
+        Deque<Integer> reversePostOrder = new ArrayDeque<>();
+        for (int v : postorder) {
+            reversePostOrder.addFirst(v);
+        }
+
+        return reversePostOrder;
     }
 
     public String toString() {
