1c7ff01ec33cb08b2efa9ac56d951c8e2dffc545
Dmitriy Panasiuk <dmitriy.panasyuk@gmail.com>
2017-04-03 22:53:20 +0300

    Code

--- tasks/MinHeap.java
+++ tasks/MinHeap.java
@@ -1,4 +1,83 @@
-package PACKAGE_NAME;
+import java.util.NoSuchElementException;
 
 public class MinHeap {
+    private int maxN;
+    private int n;
+    private int[] pq;
+    private int[] qp;
+    private Double[] keys;
+
+    public MinHeap(int maxN) {
+        this.maxN = maxN;
+        n = 0;
+        keys = new Double[maxN + 1];
+        pq = new int[maxN + 1];
+        qp = new int[maxN + 1];
+        for (int i = 0; i <= maxN; i++) {
+            qp[i] = -1;
+        }
+    }
+
+    public boolean isEmpty() {
+        return n == 0;
+    }
+
+    public boolean contains(int i) {
+        return qp[i] != -1;
+    }
+
+    public void insert(int i, Double key) {
+        if (contains(i)) throw new IllegalArgumentException("index is already in heap");
+        n++;
+        qp[i] = n;
+        pq[n] = i;
+        keys[i] = key;
+        swim(n);
+    }
+
+    public int delMin() {
+        if (n == 0) throw new NoSuchElementException("Heap is empty");
+        int min = pq[1];
+        exch(1, n--);
+        sink(1);
+        qp[min] = -1;
+        keys[min] = null;
+        pq[n + 1] = -1;        // not needed
+        return min;
+    }
+
+    public void decreaseKey(int i, Double key) {
+        if (!contains(i)) throw new NoSuchElementException("index is not in the heap");
+        keys[i] = key;
+        swim(qp[i]);
+    }
+
+    private boolean greater(int i, int j) {
+        return keys[pq[i]] > keys[pq[j]];
+    }
+
+    private void exch(int i, int j) {
+        int swap = pq[i];
+        pq[i] = pq[j];
+        pq[j] = swap;
+        qp[pq[i]] = i;
+        qp[pq[j]] = j;
+    }
+
+    private void swim(int k) {
+        while (k > 1 && greater(k / 2, k)) {
+            exch(k, k / 2);
+            k = k / 2;
+        }
+    }
+
+    private void sink(int k) {
+        while (2 * k <= n) {
+            int j = 2 * k;
+            if (j < n && greater(j, j + 1)) j++;
+            if (!greater(k, j)) break;
+            exch(k, j);
+            k = j;
+        }
+    }
 }
--- tasks/ShortestPaths.java
+++ tasks/ShortestPaths.java
@@ -1,4 +1,24 @@
-package PACKAGE_NAME;
-
 public class ShortestPaths {
+    public static void main(String[] args) {
+        int V = 10;
+        EdgeWeightedDigraph exampleGraph = new EdgeWeightedDigraph(V);
+        exampleGraph.addEdge(0, 1, 7);exampleGraph.addEdge(0, 2, 3);exampleGraph.addEdge(1, 3, 8);exampleGraph.addEdge(1, 4, 3);
+        exampleGraph.addEdge(2, 1, 1);exampleGraph.addEdge(2, 3, 2);exampleGraph.addEdge(2, 4, 1);exampleGraph.addEdge(3, 5, 2);
+        exampleGraph.addEdge(3, 0, 2);exampleGraph.addEdge(4, 3, 9);exampleGraph.addEdge(4, 5, 4);exampleGraph.addEdge(5, 2, 4);
+        exampleGraph.addEdge(6, 7, 10);exampleGraph.addEdge(4, 7, 1);exampleGraph.addEdge(8, 9, 3);
+        double NO = Double.POSITIVE_INFINITY;
+        double[][] solutionMatrix = {
+                {0, 4, 3, 5, 4, 7, NO, 5, NO, NO},
+                {10, 0, 11, 8, 3, 7, NO, 4, NO, NO},
+                {4, 1, 0, 2, 1, 4, NO, 2, NO, NO},
+                {2, 6, 5, 0, 6, 2, NO, 7, NO, NO},
+                {11, 9, 8, 9, 0, 4, NO, 1, NO, NO},
+                {8, 5, 4, 6, 5, 0, NO, 6, NO, NO},
+                {NO, NO, NO, NO, NO, NO, 0, 10, NO, NO},
+                {NO, NO, NO, NO, NO, NO, NO, 0, NO, NO},
+                {NO, NO, NO, NO, NO, NO, NO, NO, 0, 3},
+                {NO, NO, NO, NO, NO, NO, NO, NO, NO, 0}};
+
+
+    }
 }
