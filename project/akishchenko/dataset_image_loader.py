ef4a53b3dc07a87a42c7f0632a5dd7ec35fbbf80
Andrew Kishchenko <psspectre@gmail.com>
2017-05-21 21:52:10 +0300

    more faster dataset image loader

--- project/akishchenko/dataset_image_loader.py
+++ project/akishchenko/dataset_image_loader.py
@@ -1,26 +1,28 @@
 #load lun.ua dataset images from csv file
 import csv
 import requests
-from PIL import Image
-from io import BytesIO
+import shutil
 
-def get_file_name(url, cluster, folder='images'):
+def get_file_name(url, cluster, folder='images2'):
     split = url.split('/')
     return "{0}/({1})_{2}".format(folder, cluster, split[-1])
 
 def load_image(url, cluster):
-    response = requests.get(url)
-    img = Image.open(BytesIO(response.content))
     fname = get_file_name(url, cluster)
-    img.save(fname)
+    response = requests.get(url, stream=True)
+    with open(fname, 'wb') as out_file:
+        shutil.copyfileobj(response.raw, out_file)
+    del response
     print('downloaded and saved:' , fname)
 
 def load_csv(fname):
     with open(fname) as csvfile:
         reader = csv.DictReader(csvfile)
+        count = 0
         for row in reader:
-            print(row['image_url'], row['cluster_id'])
+            print("[{0}] load: {1} {2}".format(count, row['image_url'], row['cluster_id']))
             load_image(row['image_url'], row['cluster_id'])
+            count += 1
 
 if __name__ == '__main__':
     fname = 'images/lun_image_clusters_training_set.csv'
